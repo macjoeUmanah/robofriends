9a230c78cef3b2e34f9970781b772fc0af1e09db
macjoeUmanah <macjoeu5@gmail.com>
2018-10-11 01:24:15 -1200

    created a parent componet CardList to render the Cards. Also made the Cards rendering more efficient by looping through the robots array

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Card from './Card';
+import CardList from './CardList';
 import {robots} from './robots';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -9,11 +9,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
 
-	<div>
-		<Card name={robots[0].name} id={robots[0].id} email={robots[0].email}/>
-		<Card name={robots[1].name} id={robots[1].id} email={robots[1].email}/>
-		<Card name={robots[2].name} id={robots[2].id} email={robots[2].email}/>
-	</div>
+	<CardList robots={robots}/>
+
+	
 
 
 
