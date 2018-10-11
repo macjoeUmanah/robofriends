fe24c59624e816f1d6896e14c83eaa165ac05f9c
macjoeUmanah <macjoeu5@gmail.com>
2018-10-11 00:24:26 -1200

    created the card component with users from the robots.js file

---
+++ src/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import tachyons from 'tachyons';
+
+
+const Card=({name,email,id})=>{
+
+
+	return(
+
+
+		<div className='bg-light-green dib br3 pa3 grow ma2 bw2 shadow-5 tc'>
+
+		   <img src={'https://robohash.org/hheee'} alt={'robots'}/>
+
+		   <div>
+
+		      <h1>{name}</h1>
+
+
+
+		      <p>{email}</p>
+
+
+		   </div>
+
+
+
+		</div>
+
+
+
+	);
+}
+
+
+export default Card;
