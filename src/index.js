415a7d60110f66b4c06fb24dfa55da0da629e11f
macjoe umanah <macjoeu780@gmail.com>
2018-10-10 23:24:34 -1200

    added the initial files for the app

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+import MySection from './MySection';
+import MyComponent from './MyComponent';
+import MyState from './State';
+import ChangeState from './Change';
+import MyButton from './Default';
+import Simple from './Simple';
+import MyContainer from './MyContainer';
+import MyList from './MyList';
+
+
+
+
+
+	
+
+
+
+	render((
+
+
+
+		<MyList />
+
+
+	),document.getElementById('root')
+
+);
+
+
+
+
+
+
+
+//setTimeout(()=>{Changed.setState({heading:'this is awesomely',content:'Done'})},4000);
+
+
